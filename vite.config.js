fb535ef951f5b57d64323ccd286a156854983cd1
barbosaemilio <barbosaemiliob@gmail.com>
2026-04-08 20:09:44 -0300

    fix vite base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/nome-do-seu-repositorio/',
+  base: '/github_profile/',
   plugins: [react()],
 })
